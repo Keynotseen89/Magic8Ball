b72fe6aecb9390400d56e0321275e54c48edb8b8
Keynotseen89 <ksintora@yahoo.com>
2020-10-01 21:36:46 -0700

    Added function to shake phone to interact with 8 ball

--- app/src/main/java/com/example/magic8ball/MainActivity.java
+++ app/src/main/java/com/example/magic8ball/MainActivity.java
@@ -3,8 +3,15 @@ package com.example.magic8ball;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
@@ -17,14 +24,20 @@ import android.widget.TextView;
 
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private static final String TAG = "MainActivity";
     private final String TEXT_CONTENTS = "TextContents";
     private ImageButton magicBtnAnswer;
     private ImageView triangleAnswer;
-    private EditText questionAsked;
     private TextView answerOutput;
+    private SensorManager sensorManager;
+    private Sensor accelerometerSensor;
+    private boolean isAccelerometerSensorAvailable, itIsNotFirstTime = false;
+    private float currentX, currentY, currentZ, lastX, lastY, lastZ;
+    private float xDifference, yDifference, zDifference;
+    private float shakeThreshold = 5f;
+    private Vibrator vibrator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,26 +45,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Get notified when the user has clicked on the button.
-        magicBtnAnswer = (ImageButton) findViewById(R.id.btnAnswer);
-        triangleAnswer = (ImageView) findViewById(R.id.imageViewAnswer);
-        questionAsked = (EditText) findViewById(R.id.editText);
-        answerOutput = (TextView) findViewById(R.id.tvAnswer);
+        magicBtnAnswer = findViewById(R.id.btnAnswer);
+        triangleAnswer = findViewById(R.id.imageViewAnswer);
+        answerOutput = findViewById(R.id.tvAnswer);
 
         View.OnClickListener ourOnClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.i("magic", "clicked on Magic 8 ball");
-                questionAsked.setText("");
-                makeMeShake(magicBtnAnswer, 20, 5);
-                Animation aniRotateClk = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_clockwise);
-                Animation fadAnim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fadin_animation);
-                triangleAnswer.startAnimation(aniRotateClk);
-                answerOutput.startAnimation(fadAnim);
-                answerOutput.setText(randomAnswer());
+                eightBallEffect();
             }
         };
         magicBtnAnswer.setOnClickListener(ourOnClickListener);
 
+        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
+            accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            isAccelerometerSensorAvailable = true;
+        } else {
+            isAccelerometerSensorAvailable = false;
+
+        }
+    }
+
+    public void eightBallEffect() {
+        makeMeShake(magicBtnAnswer, 20, 5);
+        Animation aniRotateClk = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_clockwise);
+        Animation fadAnim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fadin_animation);
+        triangleAnswer.startAnimation(aniRotateClk);
+        answerOutput.startAnimation(fadAnim);
+        answerOutput.setText(randomAnswer());
     }
 
     public static View makeMeShake(View view, int duration, int offset) {
@@ -64,7 +89,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private String randomAnswer() {
-        String answers[] = new String[]{"It is certain", "It is decidedly so", "Without a doubt", "Yes - definitely",
+        String[] answers = new String[]{"It is certain", "It is decidedly so", "Without a doubt", "Yes - definitely",
                 "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes",
                 "Reply hazy, try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again",
                 "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"};
@@ -83,5 +108,56 @@ public class MainActivity extends AppCompatActivity {
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         outState.putString(TEXT_CONTENTS, answerOutput.getText().toString());
         super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        currentX = event.values[0];
+        currentY = event.values[1];
+        currentZ = event.values[2];
+
+        if (itIsNotFirstTime) {
+            xDifference = Math.abs(lastX - currentX);
+            yDifference = Math.abs(lastY - currentY);
+            zDifference = Math.abs(lastZ - currentZ);
+
+            if ((xDifference > shakeThreshold && yDifference > shakeThreshold) ||
+                    (xDifference > shakeThreshold && zDifference > shakeThreshold) ||
+                    (yDifference > shakeThreshold && zDifference > shakeThreshold)) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    vibrator.vibrate(VibrationEffect.createOneShot(600, VibrationEffect.DEFAULT_AMPLITUDE));
+                    eightBallEffect();
+                } else {
+                    vibrator.vibrate(500);
+                    //deprecated in API.26
+                }
+            }
+        }
+        lastX = currentX;
+        lastY = currentY;
+        lastZ = currentZ;
+        itIsNotFirstTime = true;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (isAccelerometerSensorAvailable == true) {
+            sensorManager.registerListener(this, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (isAccelerometerSensorAvailable == false) {
+            sensorManager.unregisterListener(this);
+        }
     }
 }
